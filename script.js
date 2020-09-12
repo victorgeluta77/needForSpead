f0c587cf7c47fc3b1a8f153cf78220412b7bc68f
Victor Heliuta <victorgeluta@gmail.com>
2020-09-12 18:44:03 +0300

    tachky my first game

---
+++ script.js
@@ -0,0 +1,128 @@
+ const score = document.querySelector('.score'),
+       start = document.querySelector('.start'),
+       gameArea = document.querySelector('.gameArea'),
+       car = document.createElement('div');
+
+       car.classList.add('car');
+
+//  start.onclick = ()=>{
+//      start.classList.add('hide');
+//  }
+
+ start.addEventListener('click',startGame);
+ document.addEventListener('keydown',startRun);
+ document.addEventListener('keyup',stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false,
+};
+
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3,
+    traffic:3
+}
+
+
+function getQuantityElements(heightElement){
+   return document.documentElement.clientHeight / heightElement + 1;
+}
+
+
+ function startGame(){
+    start.classList.add('hide');
+    gameArea.classList.remove('hide');
+    gameArea.classList.add('show');
+
+    for(let i=0; i < getQuantityElements(100);i++){
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 75) + 'px';
+        line.y = i*100;
+        gameArea.appendChild(line);
+    }
+
+    for(let i=0; i < getQuantityElements(100*setting.traffic);i++){
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100*setting.traffic * (i +1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background  = 'transparent url(./image/enemy.png) center / cover no-repeat';
+        console.log(enemy.style.background);
+        gameArea.appendChild(enemy);
+    }
+
+
+    setting.start = true;
+    gameArea.appendChild(car);
+    setting.x = car.offsetLeft; // координати car - зліва; offsetRight - справа
+    setting.y = car.offsetTop;
+    requestAnimationFrame(playGame); // функція запускає в браузері Анімацію, робить це плавно без великого навантаження на браузер
+ }
+
+ function playGame(){
+     console.log('Play Game !');
+     moveRoad();
+     moveEnemy();
+     if(setting.start){
+         if(keys.ArrowLeft && setting.x > 0){
+             setting.x-=setting.speed;
+         }
+         if(keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)){
+            setting.x+=setting.speed;
+        }
+       car.style.left = setting.x + 'px';
+       if(keys.ArrowUp && setting.y >0){
+        setting.y-=setting.speed;
+        }
+        if(keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)){
+        setting.y+=setting.speed;
+         }
+         car.style.top = setting.y + 'px';
+         requestAnimationFrame(playGame); // рекурсія - функція запускає сама себе
+       }
+     }
+     
+
+ function startRun(event){
+    event.preventDefault();
+    console.log(event.key);
+    keys[event.key] = true;
+     
+ }
+ function stopRun(event){
+    event.preventDefault();
+    keys[event.key] = false;
+ }
+
+function moveRoad(){
+    let lines = document.querySelectorAll('.line');
+    lines.forEach((line,index)=>{
+            line.y += setting.speed;
+            line.style.top = line.y + 'px';
+
+            if(line.y >= document.documentElement.clientHeight){
+                line.y = -100 ;
+            }
+
+    })
+
+}
+
+function moveEnemy(){
+    let enemys = document.querySelectorAll('.enemy');
+    enemys.forEach((item)=>{
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+    if(item.y>= document.documentElement.clientHeight){
+        item.y = -100 * setting.traffic;
+        item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    }
+});
+
+};
